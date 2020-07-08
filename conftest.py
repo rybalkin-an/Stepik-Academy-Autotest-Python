145bd38af9570ef02a4c0940ce25c7f09f805c34
rybalkin-an <rybalkine@gmail.com>
2020-07-08 23:40:26 +0400

    Homework week 4 hard DL
    Mistakes were fixed

--- conftest.py
+++ conftest.py
@@ -1,21 +1,18 @@
 ﻿import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 
 def pytest_addoption(parser):
-    parser.addoption('--language', action='store', default='ru',
+    parser.addoption('--language', action='store', default=None,
                      help="Choose language: ru or es")
+
 
 @pytest.fixture(scope="function")
 def browser(request):
     language = request.config.getoption('language')
-    browser = webdriver.Chrome()
-    link = f"http://selenium1py.pythonanywhere.com/{language}/catalogue/coders-at-work_207/"
-    if language == "ru":
-        browser.get(link)
-    elif language == "es":
-        browser.get(link)
-    else:
-        raise pytest.UsageError("--language= should be ru or es")
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': language})
+    browser = webdriver.Chrome(options=options)
     yield browser
     browser.quit()
--- test_items.py
+++ test_items.py
@@ -2,8 +2,20 @@ import time
 
 
 def test_customer_should_see_add_to_basket_button(browser):
-    browser.find_element_by_xpath("//form[@id='add_to_basket_form']//button[contains(@type,'submit')]")
-    time.sleep(30)
+    browser.get("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/")
     language_actual_result = browser.find_element_by_xpath(
-        "//form[@id='add_to_basket_form']//button[contains(@type,'submit')]").text
-    assert language_actual_result == "Añadir al carrito"
+    "//form[@id='add_to_basket_form']//button[contains(@type,'submit')]")
+    time.sleep(10)
+    add_to_basket_button_is_active = language_actual_result.is_enabled()
+    assert add_to_basket_button_is_active == True, \
+            f"Such an error, 'add to basket' is not rendered"
+
+
+def test_customer_should_see_add_to_basket_button_2(browser):
+    browser.get("http://selenium1py.pythonanywhere.com/en-gb/catalogue/hacking-exposed-wireless_208/")
+    language_actual_result = browser.find_element_by_xpath(
+    "//form[@id='add_to_basket_form']//button[contains(@type,'submit')]")
+    time.sleep(10)
+    add_to_basket_button_is_active = language_actual_result.is_enabled()
+    assert add_to_basket_button_is_active == True, \
+            f"Such an error, 'add to basket' is not rendered"
