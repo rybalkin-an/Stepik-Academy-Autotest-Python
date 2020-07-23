9d2e14ce053bec79d0de5f978f53155e8cab5be3
Aleksei Rybalkin <48929168+rybalkin-an@users.noreply.github.com>
2020-07-23 14:55:07 +0400

    updated link

--- test_login_page.py
+++ test_login_page.py
@@ -2,14 +2,14 @@ from pages.login_page import LoginPage
 
 
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    link = "http://selenium1py.pythonanywhere.com/accounts/login/"
     page = LoginPage(browser, link)
     page.open()
     page.should_be_login_page()
 
 
 def test_user_can_register(browser,):
-    login_link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    login_link = "http://selenium1py.pythonanywhere.com/accounts/login/"
     login_page = LoginPage( browser, login_link )
     login_page.open()
     login_page.register_new_user()
