f005af83d3801fd1aab1a4dd238b9b0e653be610
Aleksei Rybalkin <48929168+rybalkin-an@users.noreply.github.com>
2020-07-23 14:55:29 +0400

    Update test_login_page.py

--- test_login_page.py
+++ test_login_page.py
@@ -10,7 +10,7 @@ def test_guest_can_go_to_login_page(browser):
 
 def test_user_can_register(browser,):
     login_link = "http://selenium1py.pythonanywhere.com/accounts/login/"
-    login_page = LoginPage( browser, login_link )
+    login_page = LoginPage(browser, login_link)
     login_page.open()
     login_page.register_new_user()
     login_page.should_be_authorized_user()
